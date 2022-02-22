01f24868dbe062192e6097e8d5d3fb94015de480
Billy19991110 <96582258+Billy19991110@users.noreply.github.com>
2022-02-22 15:56:25 +0800

    Add files via upload

--- test_v1.sql
+++ test_v1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： localhost:8889
--- 產生時間： 2022 年 02 月 21 日 07:25
+-- 產生時間： 2022 年 02 月 22 日 07:50
 -- 伺服器版本： 5.7.34
 -- PHP 版本： 7.4.21
 
@@ -29,18 +29,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `buy` (
   `byID` int(4) NOT NULL COMMENT '主鍵',
-  `userID` int(4) NOT NULL COMMENT '使用者ＩＤ',
+  `users_id` int(11) NOT NULL COMMENT '使用者ＩＤ',
   `productID` int(4) NOT NULL COMMENT '產品ＩＤ',
-  `productNUM` int(3) NOT NULL COMMENT '購買個數'
+  `productNUM` int(11) NOT NULL COMMENT '購買個數'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- 傾印資料表的資料 `buy`
 --
 
-INSERT INTO `buy` (`byID`, `userID`, `productID`, `productNUM`) VALUES
-(1, 1, 2, 1),
-(2, 1, 1, 1);
+INSERT INTO `buy` (`byID`, `users_id`, `productID`, `productNUM`) VALUES
+(2, 2, 33, 4);
 
 -- --------------------------------------------------------
 
@@ -49,8 +48,8 @@ INSERT INTO `buy` (`byID`, `userID`, `productID`, `productNUM`) VALUES
 --
 
 CREATE TABLE `nation` (
-  `nationID` int(4) NOT NULL COMMENT '國家ＩＤ',
-  `nationName` varchar(30) NOT NULL COMMENT '國家名稱'
+  `nationID` int(11) NOT NULL COMMENT '國家ＩＤ',
+  `nationName` varchar(100) NOT NULL COMMENT '國家名稱'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='國家表';
 
 --
@@ -68,12 +67,12 @@ INSERT INTO `nation` (`nationID`, `nationName`) VALUES
 --
 
 CREATE TABLE `picture` (
-  `productID` int(4) NOT NULL COMMENT '產品ＩＤ',
-  `pictureSeat1` varchar(30) NOT NULL COMMENT '圖片位子',
-  `pictureSeat2` varchar(30) NOT NULL COMMENT '圖片位子',
-  `pictureSeat3` varchar(30) NOT NULL COMMENT '圖片位子',
-  `pictureSeat4` varchar(30) NOT NULL COMMENT '圖片位子',
-  `pictureSeat5` varchar(30) NOT NULL COMMENT '圖片位子'
+  `productID` int(11) NOT NULL COMMENT '產品ＩＤ',
+  `pictureSeat1` varchar(100) NOT NULL COMMENT '圖片位子',
+  `pictureSeat2` varchar(100) NOT NULL COMMENT '圖片位子',
+  `pictureSeat3` varchar(100) NOT NULL COMMENT '圖片位子',
+  `pictureSeat4` varchar(100) NOT NULL COMMENT '圖片位子',
+  `pictureSeat5` varchar(100) NOT NULL COMMENT '圖片位子'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='圖片表';
 
 --
@@ -81,45 +80,53 @@ CREATE TABLE `picture` (
 --
 
 INSERT INTO `picture` (`productID`, `pictureSeat1`, `pictureSeat2`, `pictureSeat3`, `pictureSeat4`, `pictureSeat5`) VALUES
-(35, '/img/DVD/15周年con.jpg', '', '', '', ''),
-(36, '/img/DVD/2015小島祭.jpg', '', '', '', ''),
-(37, '/img/DVD/2019單獨con.jpg', '', '', '', ''),
-(38, '/img/DVD/第10回總選.jpg', '', '', '', ''),
-(39, '/img/DVD/第8回紅白.jpg', '', '', '', ''),
-(4, '/img/手燈/2020早安官燈.jpg', '', '', '', ''),
-(5, '/img/手燈/2021早安官燈.jpg', '', '', '', ''),
-(1, '/img/手燈/48官燈type1_1.jpg', '/img/手燈/48官燈type1_2.jpg', '/img/手燈/48官燈type1_3.jpg', '/img/手燈/48官燈type1_4.jpg', '/img/手燈/48官燈type1_5.jpg'),
-(2, '/img/手燈/48官燈type2_1.jpg', '/img/手燈/48官燈type2_2.jpg', '', '', ''),
-(3, '/img/手燈/48官燈type3_1.jpg', '/img/手燈/48官燈type3_2.jpg', '/img/手燈/48官燈type3_3.jpg', '/img/手燈/48官燈type3_4.jpg', '/img/手燈/48官燈type3_5.jpg'),
-(14, '/img/推巾/15周年單獨con.jpg', '', '', '', ''),
-(15, '/img/推巾/2018感謝祭.jpg', '', '', '', ''),
-(16, '/img/推巾/2019全國tour.jpg', '', '', '', ''),
-(17, '/img/推巾/2019春con.jpg', '', '', '', ''),
-(19, '/img/推巾/2020RH.jpg', '', '', '', ''),
-(18, '/img/推巾/2020新體感live.jpg', '', '', '', ''),
-(20, '/img/推巾/2022新春con.jpg', '', '', '', ''),
-(25, '/img/推巾/STU2021聖誕個別推巾_1.jpg', '/img/推巾/STU2021聖誕個別推巾_2.jpg', '/img/推巾/STU2021聖誕個別推巾_3.jpg', '/img/推巾/STU2021聖誕個別推巾_4.jpg', '/img/推巾/STU2021聖誕個別推巾_5.jpg'),
-(24, '/img/推巾/STU4周年推巾.jpg', '', '', '', ''),
-(26, '/img/推巾/STU武道館con推巾.jpg', '', '', '', ''),
-(27, '/img/推巾/STU船公演推巾.jpg', '', '', '', ''),
-(21, '/img/推巾/早安聯名推巾.jpg', '', '', '', ''),
-(23, '/img/推巾/第10回總選.jpg', '', '', '', ''),
-(22, '/img/推巾/第8回紅白.jpg', '', '', '', ''),
-(28, '/img/推扇/STU2021聖誕推扇_1.jpg', '/img/推扇/STU2021聖誕推扇_2.jpg', '/img/推扇/STU2021聖誕推扇_3.jpg', '/img/推扇/STU2021聖誕推扇_4.jpg', '/img/推扇/STU2021聖誕推扇_5.jpg'),
-(29, '/img/推扇/STU大正個人推扇_1.jpg', '/img/推扇/STU大正個人推扇_2.jpg', '/img/推扇/STU大正個人推扇_3.jpg', '/img/推扇/STU大正個人推扇_4.jpg', '/img/推扇/STU大正個人推扇_5.jpg'),
-(30, '/img/推扇/T4推扇_1.jpg', '/img/推扇/T4推扇_2.jpg', '', '', ''),
-(31, '/img/推扇/T8推扇_1.jpg', '/img/推扇/T8推扇_2.jpg', '', '', ''),
-(32, '/img/推扇/TA推扇_1.jpg', '/img/推扇/TA推扇_2.jpg', '', '', ''),
-(33, '/img/推扇/TB推扇_1.jpg', '/img/推扇/TB推扇_2.jpg', '', '', ''),
-(34, '/img/推扇/TK推扇.jpg', '/img/推扇/TK推扇_2.jpg', '', '', ''),
-(6, '/img/衣服/15周年單獨con.jpg', '', '', '', ''),
-(7, '/img/衣服/2018單獨con_正.jpg', '/img/衣服/2018單獨con_反.jpg', '', '', ''),
-(8, '/img/衣服/2019全國tour.jpg', '', '', '', ''),
-(9, '/img/衣服/2022新春con_正.jpg', '/img/衣服/2022新春con_反.jpg', '', '', ''),
-(12, '/img/衣服/STU4周年T.jpg', '', '', '', ''),
-(13, '/img/衣服/STU武道館con.jpg', '', '', '', ''),
-(10, '/img/衣服/早安聯名T.jpg', '', '', '', ''),
-(11, '/img/衣服/第10回總選T.jpg', '', '', '', '');
+(25, '/img/DVD/15周年con.jpg', '/img/DVD/15周年con敘述.jpg', '/img/DVD/15周年con規格.jpg', '', ''),
+(26, '/img/DVD/2015小島祭.jpg', '/img/DVD/2015小島祭敘述.jpg', '/img/DVD/2015小島祭規格.jpg', '', ''),
+(27, '/img/DVD/2019單獨con.jpg', '/img/DVD/2019單獨con敘述1.jpg', '/img/DVD/2019單獨con規格.jpg', '', ''),
+(28, '/img/DVD/第10回總選.jpg', '/img/DVD/第10回總選敘述.jpg', '/img/DVD/', '', ''),
+(29, '/img/DVD/第8回紅白.jpg', '/img/DVD/第8回紅白敘述.jpg', '/img/DVD/第8回紅白規格.jpg', '', ''),
+(4, '/img/手燈/2020早安官燈.jpg', '/img/手燈/2020早安官燈敘述與規格.jpg', '', '', ''),
+(5, '/img/手燈/2021早安官燈.jpg', '/img/手燈/2021早安官燈敘述與規格.jpg', '', '', ''),
+(1, '/img/手燈/48官燈type1_1.jpg', '/img/手燈/48官燈type1敘述.jpg', '/img/手燈/48官燈type1規格.jpg', '', ''),
+(2, '/img/手燈/48官燈type2_1.jpg', '/img/手燈/48官燈type2敘述.jpg', '/img/手燈/48官燈type2規格.jpg', '', ''),
+(3, '/img/手燈/48官燈type3_1.jpg', '/img/手燈/48官燈type3敘述.jpg', '/img/手燈/48官燈type3規格.jpg', '', ''),
+(14, '/img/推巾/15周年單獨con.jpg', '/img/推巾/15周年單獨con敘述.jpg', '/img/推巾/15周年單獨con規格.jpg', '', ''),
+(16, '/img/推巾/2020RH.jpg', '/img/推巾/2020RH敘述.jpg', '/img/推巾/2020RH規格.jpg', '', ''),
+(15, '/img/推巾/2020新體感live.jpg', '/img/推巾/2020新體感live敘述.jpg', '/img/推巾/2020新體感live規格.jpg', '', ''),
+(17, '/img/推巾/2022新春con.jpg', '/img/推巾/2022新春con敘述.jpg', '', '', ''),
+(18, '/img/推扇/STU2021聖誕推扇_1.jpg', '/img/推扇/STU2021聖誕個別推扇敘述.jpg', '/img/推扇/STU2021聖誕個別推扇規格.jpg', '', ''),
+(19, '/img/推扇/STU大正個人推扇_1.jpg', '/img/推扇/STU大正個人推扇敘述.jpg', '/img/推扇/STU大正個人推扇規格.jpg', '', ''),
+(20, '/img/推扇/T4推扇_1.jpg', '/img/推扇/T4推扇敘述.jpg', '/img/推扇/T4推扇規格.jpg', '', ''),
+(21, '/img/推扇/T8推扇_1.jpg', '/img/推扇/T8推扇敘述.jpg', '/img/推扇/T8推扇規格.jpg', '', ''),
+(22, '/img/推扇/TA推扇_1.jpg', '/img/推扇/TA推扇敘述.jpg', '/img/推扇/TA推扇規格.jpg', '', ''),
+(23, '/img/推扇/TB推扇_1.jpg', '/img/推扇/TB推扇敘述.jpg', '/img/推扇/TB推扇規格.jpg', '', ''),
+(24, '/img/推扇/TK推扇.jpg', '/img/推扇/TK推扇敘述.jpg', '/img/推扇/TK推扇規格.jpg', '', ''),
+(6, '/img/衣服/15周年單獨con.jpg', '/img/衣服/15周年單獨con敘述.jpg', '/img/衣服/15周年單獨con規格.jpg', '', ''),
+(7, '/img/衣服/2018單獨con_正.jpg', '/img/衣服/2018單獨con敘述.jpg', '/img/衣服/2018單獨con規格.jpg', '', ''),
+(8, '/img/衣服/2019全國tour.jpg', '/img/衣服/2019全國tour敘述.jpg', '/img/衣服/2019全國tour規格.jpg', '', ''),
+(9, '/img/衣服/2022新春con_正.jpg', '/img/衣服/2022新春con敘述.jpg', '/img/衣服/2022新春con規格.jpg', '', ''),
+(12, '/img/衣服/STU4周年T.jpg', '/img/衣服/STU四周年T敘述.jpg', '/img/衣服/STU四周年T規格.jpg', '', ''),
+(13, '/img/衣服/STU武道館con.jpg', '/img/衣服/STU武道館con敘述.jpg', '/img/衣服/STU武道館con規格.jpg', '', ''),
+(10, '/img/衣服/早安聯名T.jpg', '/img/衣服/早安聯名T敘述.jpg', '/img/衣服/早安聯名T規格.jpg', '', ''),
+(11, '/img/衣服/第10回總選T.jpg', '/img/衣服/第10回總選T敘述.jpg', '/img/衣服/第10回總選T規格.jpg', '', ''),
+(30, '/img/韓國/BTSbag.jpg', '/img/韓國/商品敘述/BTSbag.jpg', '', '', ''),
+(31, '/img/韓國/BTSbottle.jpg', '/img/韓國/商品敘述/BTSbottle.jpg', '', '', ''),
+(32, '/img/韓國/BTSdoll.jpg', '/img/韓國/商品敘述/BTSdoll.jpg', '', '', ''),
+(33, '/img/韓國/BTShandbag.jpg', '/img/韓國/商品敘述/BTShandbag.jpg', '', '', ''),
+(34, '/img/韓國/BTSsticker.jpg', '/img/韓國/商品敘述/BTSsticker.jpg', '', '', ''),
+(35, '/img/韓國/ITZYbadge.jpg', '/img/韓國/商品敘述/ITZYbadge.jpg', '', '', ''),
+(36, '/img/韓國/ITZYlightkeyring.png', '/img/韓國/商品敘述/ITZYlightkeyring.jpg', '', '', ''),
+(37, '/img/韓國/ITZYministicker.jpg', '/img/韓國/商品敘述/ITZYministicker.jpg', '', '', ''),
+(38, '/img/韓國/ITZYstickerset.jpg', '/img/韓國/商品敘述/ITZYstickerset.jpg', '', '', ''),
+(39, '/img/韓國/ITZYstrapbadge.jpg', '/img/韓國/商品敘述/ITZYstrapbadge.jpg', '', '', ''),
+(40, '/img/韓國/TWICEcupholder.jpg', '/img/韓國/商品敘述/TWICEcupholder.jpg', '', '', ''),
+(41, '/img/韓國/TWICEfan.png', '/img/韓國/商品敘述/TWICEfan.jpg', '', '', ''),
+(42, '/img/韓國/TWICEhandbag.jpg', '/img/韓國/商品敘述/TWICEhandbag.jpg', '', '', ''),
+(43, '/img/韓國/TWICEkeyring.jpg', '/img/韓國/商品敘述/TWICEkeyring.jpg', '', '', ''),
+(44, '/img/韓國/TWICEpassportcase.jpg', '/img/韓國/商品敘述/TWICEpassportcase.jpg', '', '', ''),
+(45, '/img/韓國/YENAbag.jpg', '/img/韓國/商品敘述/YENAbag.jpg', '', '', ''),
+(46, '/img/韓國/YENAdoll.jpg', '/img/韓國/商品敘述/YENAdoll.jpg', '', '', ''),
+(47, '/img/韓國/YENAminipouch.jpg', '/img/韓國/商品敘述/YENAminipouch.jpg', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -128,10 +135,10 @@ INSERT INTO `picture` (`productID`, `pictureSeat1`, `pictureSeat2`, `pictureSeat
 --
 
 CREATE TABLE `product` (
-  `productID` int(4) NOT NULL COMMENT '產品ＩＤ',
-  `nationID` int(4) NOT NULL COMMENT '國家ＩＤ',
-  `productName` varchar(30) NOT NULL COMMENT '產品名稱',
-  `productPrice` int(4) NOT NULL COMMENT '產品價格'
+  `productID` int(11) NOT NULL COMMENT '產品ＩＤ',
+  `nationID` int(11) NOT NULL COMMENT '國家ＩＤ',
+  `productName` varchar(100) NOT NULL COMMENT '產品名稱',
+  `productPrice` int(11) NOT NULL COMMENT '產品價格'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='產品表';
 
 --
@@ -139,68 +146,76 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`productID`, `nationID`, `productName`, `productPrice`) VALUES
-(1, 1, '48官燈', 200),
-(2, 1, '48官燈', 200),
-(3, 1, '48官燈', 200),
-(4, 1, '2020早安官燈', 300),
-(5, 1, '2021早安官燈', 300),
-(6, 1, '15周年單獨con', 500),
-(7, 1, '2018單獨con', 500),
-(8, 1, '2019全國tour', 500),
-(9, 1, '2022新春con', 500),
-(10, 1, '早安聯名T', 500),
-(11, 1, '第10回總選T', 500),
-(12, 1, 'STU4周年T', 500),
-(13, 1, 'STU武道館con', 500),
-(14, 1, '15周年單獨con', 300),
-(15, 1, '2018感謝祭', 300),
-(16, 1, '2019全國tour', 300),
-(17, 1, '2019春con', 300),
-(18, 1, '2020新體感live', 300),
-(19, 1, '2020RH', 300),
-(20, 1, '2022新春con', 300),
-(21, 1, '早安聯名推巾', 300),
-(22, 1, '第8回紅白', 300),
-(23, 1, '第10回總選', 300),
-(24, 1, 'STU4周年推巾', 300),
-(25, 1, 'STU2021聖誕個別推巾', 300),
-(26, 1, 'STU武道館con推巾', 300),
-(27, 1, 'STU船公演推巾', 300),
-(28, 1, 'STU2021聖誕推扇', 300),
-(29, 1, 'STU大正個人推扇', 300),
-(30, 1, 'T4推扇', 300),
-(31, 1, 'T8推扇', 300),
-(32, 1, 'TA推扇', 300),
-(33, 1, 'TB推扇', 300),
-(34, 1, 'TK推扇', 300),
-(35, 1, '15周年con', 1000),
-(36, 1, '2015小島祭', 1000),
-(37, 1, '2019單獨con', 1000),
-(38, 1, '第10回總選', 1000),
-(39, 1, '第8回紅白', 1000);
+(1, 1, 'AKB48 official neo 12色手燈', 800),
+(2, 1, 'AKB48 official 7色造型手燈', 1100),
+(3, 1, 'AKB48 official neo 7色手燈', 700),
+(4, 1, '早安少女組。‘20 官方手燈', 1000),
+(5, 1, '早安少女組。‘21 官方手燈', 900),
+(6, 1, 'AKB48 15週年單獨演唱會紀念Ｔ恤', 850),
+(7, 1, 'AKB48 2018年單獨演唱會紀念Ｔ恤', 850),
+(8, 1, 'AKB48 2019年全國巡迴演唱會紀念Ｔ恤', 850),
+(9, 1, 'AKB48 2022年新春演唱會紀念Ｔ恤', 850),
+(10, 1, '早安少女組。Ｘ Caribadix 聯名紀念Ｔ恤', 850),
+(11, 1, 'AKB48 第10回世界選拔總選舉紀念Ｔ恤', 850),
+(12, 1, 'STU48 4週年紀念Ｔ恤', 850),
+(13, 1, 'STU48 武道館演唱會紀念Ｔ恤', 850),
+(14, 1, 'AKB48 15週年單獨演唱會紀念推巾', 600),
+(15, 1, 'AKB48 2020年新體感live紀念推巾', 600),
+(16, 1, 'AKB48 2020年Request Hour紀念推巾', 600),
+(17, 1, 'AKB48 2022年新春演唱會紀念推巾', 600),
+(18, 1, 'STU48 2021年聖誕節個別推巾', 600),
+(19, 1, 'STU48 大正風格個別推扇', 300),
+(20, 1, 'AKB48 15週年紀念個別推扇(Team4)', 300),
+(21, 1, 'AKB48 15週年紀念個別推扇(Team8)', 300),
+(22, 1, 'AKB48 15週年紀念個別推扇(TeamA)', 300),
+(23, 1, 'AKB48 15週年紀念個別推扇(TeamB)', 300),
+(24, 1, 'AKB48 15週年紀念個別推扇(TeamK)', 300),
+(25, 1, 'AKB48 15週年演唱會ＤＶＤ', 4000),
+(26, 1, 'AKB48 2015年小嶋祭演唱會ＤＶＤ', 2500),
+(27, 1, 'AKB48 2019年單獨演唱會ＤＶＤ', 4050),
+(28, 1, 'AKB48 第10回世界選拔總選舉ＤＶＤ', 4300),
+(29, 1, 'AKB48 第8回紅白歌合戰ＤＶＤ', 1650),
+(30, 2, 'BTS(防彈少年團) Hip Hop Monster手提袋', 500),
+(31, 2, 'BTS(防彈少年團) Built NY X BTS BOTTLE', 1000),
+(32, 2, 'BTS(防彈少年團) Character mini figure', 450),
+(33, 2, 'BTS(防彈少年團) MIC Drop Cable patch pouch', 450),
+(34, 2, 'BTS(防彈少年團) Hip Hop Monster布章var2', 150),
+(35, 2, 'ITZY 2021 Best Friend\'s 布章', 250),
+(36, 2, 'ITZY official 手燈鑰匙圈', 600),
+(37, 2, 'ITZY Light Ring POP-UP 迷你布章組', 200),
+(38, 2, 'ITZY Light Ring POP-UP DECO貼紙組', 200),
+(39, 2, 'ITZY 2021 Best Friend\'s 布章', 250),
+(40, 2, 'TWICE 2019 ONCE Helloween 杯套', 250),
+(41, 2, 'TWICE 2021 第4次世界巡迴演唱會 個別推扇', 200),
+(42, 2, 'TWICE 2019 ONCE Helloween 票卡包', 450),
+(43, 2, 'TWICE 2019 ONCE Helloween Lovely迷你鑰匙圈', 550),
+(44, 2, 'TWICE 2019 TWICELight Lovely護照套', 350),
+(45, 2, 'YENA(崔叡娜） YENA\'s Animal Detective提袋', 1000),
+(46, 2, 'YENA(崔叡娜) YENA\'s Animal Detective玩偶', 450),
+(47, 2, 'YENA(崔叡娜) The world of my 17 season 迷你提袋', 500);
 
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `user`
+-- 資料表結構 `users`
 --
 
-CREATE TABLE `user` (
-  `userID` int(4) NOT NULL COMMENT '會員ＩＤ',
-  `userName` varchar(30) NOT NULL COMMENT '會員名稱',
-  `userPhone` int(12) NOT NULL COMMENT '會員電話'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='會員表';
+CREATE TABLE `users` (
+  `users_id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `imageUrl` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- 傾印資料表的資料 `user`
+-- 傾印資料表的資料 `users`
 --
 
-INSERT INTO `user` (`userID`, `userName`, `userPhone`) VALUES
-(1, '許姿婷', 965806572),
-(2, '許秉純', 965802572),
-(3, '楊慈軒', 932149142),
-(4, '陳宇勝', 966210101),
-(5, '程式裁縫師', 908810969);
+INSERT INTO `users` (`users_id`, `name`, `email`, `password`, `imageUrl`, `date`) VALUES
+(2, '宇勝陳', 'tea551234@gmail.com', '109743956245694159928', 'https://lh3.googleusercontent.com/a/AATXAJxIsD-yI2k_ADTi_6r-7R5yESVM_007YaJPvL0U=s96-c', '2022-02-18 08:00:22'),
+(3, '茶耶', 'ggbbhhnn15@gmail.com', '115804666636627367647', 'https://lh3.googleusercontent.com/a-/AOh14GjDCHIHQRz_DnlkByrSRsU-1cUofpcF1iIbuDjukw=s96-c', '2022-02-18 08:13:31');
 
 --
 -- 已傾印資料表的索引
@@ -211,8 +226,8 @@ INSERT INTO `user` (`userID`, `userName`, `userPhone`) VALUES
 --
 ALTER TABLE `buy`
   ADD PRIMARY KEY (`byID`),
-  ADD KEY `userID` (`userID`),
-  ADD KEY `productID` (`productID`);
+  ADD KEY `productID` (`productID`),
+  ADD KEY `users_id` (`users_id`) USING BTREE;
 
 --
 -- 資料表索引 `nation`
@@ -237,10 +252,10 @@ ALTER TABLE `product`
   ADD KEY `productPrice` (`productPrice`);
 
 --
--- 資料表索引 `user`
+-- 資料表索引 `users`
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`userID`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`users_id`);
 
 --
 -- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
@@ -256,19 +271,19 @@ ALTER TABLE `buy`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `nation`
 --
 ALTER TABLE `nation`
-  MODIFY `nationID` int(4) NOT NULL AUTO_INCREMENT COMMENT '國家ＩＤ', AUTO_INCREMENT=7;
+  MODIFY `nationID` int(11) NOT NULL AUTO_INCREMENT COMMENT '國家ＩＤ', AUTO_INCREMENT=3;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `product`
 --
 ALTER TABLE `product`
-  MODIFY `productID` int(4) NOT NULL AUTO_INCREMENT COMMENT '產品ＩＤ', AUTO_INCREMENT=40;
+  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT COMMENT '產品ＩＤ', AUTO_INCREMENT=58;
 
 --
--- 使用資料表自動遞增(AUTO_INCREMENT) `user`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `users`
 --
-ALTER TABLE `user`
-  MODIFY `userID` int(4) NOT NULL AUTO_INCREMENT COMMENT '會員ＩＤ', AUTO_INCREMENT=6;
+ALTER TABLE `users`
+  MODIFY `users_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- 已傾印資料表的限制式
@@ -278,8 +293,8 @@ ALTER TABLE `user`
 -- 資料表的限制式 `buy`
 --
 ALTER TABLE `buy`
-  ADD CONSTRAINT `buy_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`),
-  ADD CONSTRAINT `buy_ibfk_2` FOREIGN KEY (`productID`) REFERENCES `product` (`productID`);
+  ADD CONSTRAINT `buy_ibfk_2` FOREIGN KEY (`productID`) REFERENCES `product` (`productID`),
+  ADD CONSTRAINT `buy_ibfk_3` FOREIGN KEY (`users_id`) REFERENCES `users` (`users_id`);
 
 --
 -- 資料表的限制式 `picture`
